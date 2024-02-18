f6c0042453ffb2b8e30abf67a2b8ce6307507d6d
jose <73783365+HeyListen101@users.noreply.github.com>
2024-02-18 15:01:03 +0800

    Apply Admin class changes based from diagram

--- ConsultationManagement/Admin.cs
+++ ConsultationManagement/Admin.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsultationManagement
 {
-    internal class Admin
+    public class Admin
     {
+        private HashSet<Personnel> personnels;
+        public Account Account { get; private set; }
+        public Admin(string email, string password)
+        {
+            this.Account = new Account(email, password, true);    
+            this.personnels = new HashSet<Personnel>();
+        }
+        public void AddPersonnel(Personnel personnel)
+        {
+            this.personnels.Add(personnel);
+        }
+        public void RemovePersonnel(Personnel personnel)
+        {
+            this.personnels.Remove(personnel);
+        }
+        public void ViewPersonnelRequest(Personnel personnel)
+        {
+
+        }
+        public void DeleteRequest(Request request)
+        {
+
+        }
     }
 }
