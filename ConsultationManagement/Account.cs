ea585d854889312cb42f09bead52655d90869797
jose <73783365+HeyListen101@users.noreply.github.com>
2024-02-18 14:15:10 +0800

    Apply Account class changes based from diagram

--- ConsultationManagement/Account.cs
+++ ConsultationManagement/Account.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsultationManagement
 {
-    internal class Account
+    public class Account
     {
+        private string emailAddress { get; set; }
+        private string password { get; set; }
+        private bool isAdmin { get; set; }
+        private DateTime dateCreated { get; set; }
+        private bool isArchived { get; set; }
+
+        public Account(string emailAddress, string password, bool isAdmin)
+        {
+            this.emailAddress = emailAddress;
+            this.password = password;
+            this.isAdmin = isAdmin;
+            this.dateCreated = DateTime.Now;
+        }
+
+        public bool Authenticate(string email, string pw)
+        {
+            return (this.emailAddress == email && this.password == pw && !isArchived);
+        }
+
+        public void DeleteAccount()
+        {
+            this.isArchived = true;
+        }
     }
 }
